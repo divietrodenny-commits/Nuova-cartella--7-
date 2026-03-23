6efd10909708ea070dceaf6608841d48e3d1a4ad
divietrodenny-commits <divietrodenny@gmail.com>
2026-03-23 12:49:34 +0100

    aggiunto collegamento singlepruducts ad app

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Components/Home";
 import Products from "./Components/Products";
+import SingleProduct from "./Components/SingleProducts";
 import About from "./Components/About";
 import DefaultLayout from "./Components/DefaultLayout";
 
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<SingleProduct />} />
         </Route>
       </Routes>
     </BrowserRouter>
